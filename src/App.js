e32c5e2f458727973d5fe0079dbaf880e9d95d42
Luis <luisrc91@live.com>
2023-08-11 00:26:35 -0400

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
+import Footer from './components/shares/Footer'
+import Main from './components/shares/Main'
+import Principal from './components/shares/Principal'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Main></Main>
+    <Principal></Principal>
+    <Footer></Footer>
     </div>
   );
 }
